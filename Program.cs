46201c5bc7fde746bacdbdce589cd481f060113f
Vailton Carlitos Gomes <55075180+Vailton@users.noreply.github.com>
2020-03-25 20:34:29 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Calculadorgit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 0;
+            int num2 = 0;
+            string operacao = "";
+
+            Console.WriteLine("Por favor digite o primeiro número!");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Por favor digite o segundo número!");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nEscolha o uma operação! \n\n" +
+             "a ou + : adiçao \r\n" +
+            "b ou - : subtracao\r\n" +
+            "c ou * : multiplicação\r\n" +
+            "d ou / : divisão\n");
+
+            if (operacao == "a" || operacao == "+")
+            {
+                Console.WriteLine(num1 + num2);
+            }
+
+
+            Console.WriteLine(num1 + num2);
+
+        }
+    }
+}
