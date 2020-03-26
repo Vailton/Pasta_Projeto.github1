9d6814dcde2d89345a666775e72163df10ed8b18
Vailton Gomes <vailton.tech@gmail.com>
2020-03-25 21:12:04 -0300

    Segunda Atualização - Subtração

--- Program.cs
+++ Program.cs
@@ -6,15 +6,15 @@ namespace Calculadorgit
     {
         static void Main(string[] args)
         {
-            int num1 = 0;
-            int num2 = 0;
+            int numero1 = 0;
+            int numero2 = 0;
             string operacao = "";
 
             Console.WriteLine("Por favor digite o primeiro número!");
-            num1 = Convert.ToInt32(Console.ReadLine());
+            numero1 = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine("Por favor digite o segundo número!");
-            num2 = Convert.ToInt32(Console.ReadLine());
+            numero2 = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine("\nEscolha o uma operação! \n\n" +
              "a ou + : adiçao \r\n" +
@@ -22,13 +22,20 @@ namespace Calculadorgit
             "c ou * : multiplicação\r\n" +
             "d ou / : divisão\n");
 
+            operacao = Console.ReadLine();
+
             if (operacao == "a" || operacao == "+")
             {
-                Console.WriteLine(num1 + num2);
+                Console.WriteLine(numero1 + numero2);
             }
 
+            if (operacao == "b" || operacao == "-")
+            {
+                Console.WriteLine(numero1 - numero2);
+            }
 
-            Console.WriteLine(num1 + num2);
+            Console.WriteLine("\nDigite qualquer tecla para sair!");
+            Console.ReadKey();
 
         }
     }
